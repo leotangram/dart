1c6339cf63eb41c483e7858f77ef744d1ae57f01
Leonardo Omaña <leotangram@gmail.com>
2020-09-05 17:36:53 -0500

    DataTypes - numbers

---
+++ ejercicios/01_basic/02_datatypes.dart
@@ -0,0 +1,23 @@
+main() {
+  // Numbers
+  int a = 10;
+  double b = 5.5;
+  int c; // int c = null
+  double x = 10, y = 20, z = 30;
+  int _a = 30;
+  double $b = 45.95;
+
+  print($b);
+  // print(y);
+  // print(z);
+
+  // Strings
+
+  // Booleans
+
+  // Arrays
+
+  // Sets
+
+  // Maps
+}
