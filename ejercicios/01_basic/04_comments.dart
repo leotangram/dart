f2c5595ecc3909e335c677a144a1d15a1ddb1e44
Leonardo Omaña <leotangram@gmail.com>
2020-09-06 10:04:58 -0500

    Comments

---
+++ ejercicios/01_basic/04_comments.dart
@@ -0,0 +1,27 @@
+/**
+ * Este archivo tiene como objetivo los comentarios en dart
+ * [createdAdd]: Today
+ */
+
+main() {
+  // Comment
+  int a = 10; // Other comment
+
+/*
+  final people = [
+    // First person
+    'Leonardo', // [0]
+    // Second person
+    'Pedro', // [1]
+    // Third person
+    'Carlos' // [2]
+  ];
+  */
+}
+
+// Name: Person name
+// Message: Greeting
+
+/// Greeting function
+/// receive [name] & [message]
+greeting(int name, int message) {}
